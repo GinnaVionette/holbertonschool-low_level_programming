79cd6e46c36fdf67ce4c82d2f22d29762e76d079
GinnaVionette <9011@holbertonstudents.com>
2024-02-19 13:39:08 -0400

    Betty-4-preprocessor

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,2 @@
-#!/bin/bash
-gcc putchar("Programming is like building a multilingual puzzle)
+#include <stdio.h>
+int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); 
