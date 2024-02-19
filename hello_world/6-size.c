76a9f63012b121d8d689f19835b0728bbb2bc4fc
GinnaVionette <9011@holbertonstudents.com>
2024-02-19 15:54:55 -0400

    Betty-6-preprocessor

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,11 +7,17 @@
 */
 int main(void)
 {
-	printf("Size of a char %lu\n", sizeof(char));
-	printf("Size of a int %lu\n", sizeof(int));
-	printf("Size of a long int %lu\n", sizeof(long int));
-	printf("Size of a long long int %lu\n", sizeof(long long int));
-	printf("Size of a float %lu\n", sizeof(float));
+	int l;
+	char a;
+	long int k;
+	long long int f;
+	float s;
+
+	printf("Size of a char %lu\n", sizeof(a));
+	printf("Size of a int %lu\n", sizeof(l));
+	printf("Size of a long int %lu\n", sizeof(k));
+	printf("Size of a long long int %lu\n", sizeof(f));
+	printf("Size of a float %lu\n", sizeof(s));
 
 	return (0);
 }
