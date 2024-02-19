ad62bdda4cb360d880ca5012f3ff731b8e37a7f5
GinnaVionette <9011@holbertonstudents.com>
2024-02-19 15:41:49 -0400

    Betty-6-preprocessor

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("Size of a char %lu\n");
+	printf("Size of a int %lu\n");
+	printf("Size of a long int %lu\n");
+	printf("Size of a long long int %lu\n");
+	printf("Size of a float %lu\n");
+
+	return (0);
+}
