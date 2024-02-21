4724d266377ff0dd9b06874de4e52510fbd057d6
GinnaVionette <9011@holbertonstudents.com>
2024-02-21 09:12:17 -0400

    Variables-9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints a serie of numbers with commas
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
