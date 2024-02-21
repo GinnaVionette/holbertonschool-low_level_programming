d51230f50280fae6f298d23bae84435f2846ca71
GinnaVionette <9011@holbertonstudents.com>
2024-02-21 15:37:07 -0400

    Functions-2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -12,10 +12,11 @@ void print_alphabet_x10(void)
 	int n = 0;
 	char c;
 
-	while (n++ <= 9)
+	while (n <= 9)
 	{
-		for (c = 'a'; n <= 'z'; c++)
+		for (c = 'a'; c <= 'z'; c++)
 			_putchar(c);
 		_putchar('\n');
+		n++;
 	}
 }
