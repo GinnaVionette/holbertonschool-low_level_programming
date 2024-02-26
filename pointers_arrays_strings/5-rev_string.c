6abea1846d3590e41c61426dbcc56d5c07adf938
GinnaVionette <9011@holbertonstudents.com>
2024-02-26 12:56:49 -0400

    Pointers_arrays_and_strings-5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - reverses string
+ * @s: string to be modified
+ */
+void rev_string(char *s)
+{
+	int n, c, t;
+	char *a, aux;
+
+	a = s;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (t = 1; t < c; t++)
+	{
+		a++;
+	}
+
+	for (n = 0; n < (c / 2); n++)
+	{
+		aux = s[n];
+		s[n] = *a;
+		*a = aux;
+		a--;
+	}
+}
