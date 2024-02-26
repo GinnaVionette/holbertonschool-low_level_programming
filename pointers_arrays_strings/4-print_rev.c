646d449ccd4a577c23e2ce5bf5dd4df444a2ce38
GinnaVionette <9011@holbertonstudents.com>
2024-02-26 12:02:04 -0400

    Pointers_arrays_and_strings-4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -3,5 +3,24 @@
 
 /**
  * print_rev - prints string in reverse
- * @s: string to print in reverse
+ * @s: the string to print
  *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i, j, len;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
+}
