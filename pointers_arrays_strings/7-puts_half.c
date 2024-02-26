47df4e49e3157ac6adfc14669ec6d6a40eb441fb
GinnaVionette <9011@holbertonstudents.com>
2024-02-26 14:02:47 -0400

    Fuctions_arrays_and_strings-7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: the string to print
+ */
+void puts_half(char *str)
+{
+	int e = 0;
+	int g;
+
+	while (str[e] != '\0')
+	{
+		e++;
+	}
+
+	if (e % 2 == 1)
+	{
+		g = (e - 1) / 2;
+		g += 1;
+	}
+	else
+	{
+		g = e / 2;
+	}
+
+	for (g = 5; g < e; g++)
+	{
+		_putchar(str[g]);
+	}
+
+	_putchar('\n');
+}
