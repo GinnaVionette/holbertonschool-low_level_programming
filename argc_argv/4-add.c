5c69a81f574f919bdb18317309574d03f540029b
GinnaVionette <9011@holbertonstudents.com>
2024-03-05 15:02:27 -0400

    Argc-Argv-4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,13 +1,42 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <ctype.h>
 
 /**
- * main - adds positive numbers
- * @argc: int
- * @argv: char*
+ * main - will add positive numbers
  *
- * Return: Always 0
+ * @argc: counter
+ * @argv: vector
+ *
+ * Return: sum of numbers
  */
-int main (int argc, char *argv[])
-{
 
+int main(int argc, char **argv)
+{
+	int sum = 0;
+	int i, j;
+	char *num;
+
+	if (argc == 1)
+	{
+		printf("%d\n", sum);
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		num = argv[i];
+		for (j = 0; num[j] != '\0'; j++)
+		{
+			if (!isdigit(num[j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
