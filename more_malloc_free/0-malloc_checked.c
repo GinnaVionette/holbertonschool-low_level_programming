daab0651cbebe4885ecc6752e4f0cadf3eae131d
GinnaVionette <9011@holbertonstudents.com>
2024-03-07 10:26:54 -0400

    more_malloc_free-0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: memory to be allocated
+ *
+ * Return: pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p = NULL;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
---
+++ more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This is the README file for the HOlberton Exercises "More malloc, free."
