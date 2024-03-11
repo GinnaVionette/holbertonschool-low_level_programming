ab0c30fc82ec14a402f9d5750adc74a4afbc2c99
GinnaVionette <9011@holbertonstudents.com>
2024-03-11 13:15:50 -0400

    1-init_dog-4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * new_dog - makes new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
