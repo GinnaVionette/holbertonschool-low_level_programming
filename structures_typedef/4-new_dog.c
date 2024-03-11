ab7ffdee7048843e95e9cb9766d854bc88ae7fb5
GinnaVionette <9011@holbertonstudents.com>
2024-03-11 13:32:11 -0400

    1-init_dog-4.1

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * new_dog - makes new dog
@@ -7,8 +8,30 @@
  * @age: age of dog
  * @owner: owner of dog
  *
- * Return: new dog
+ * Return: a
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *a;
 
+	a = malloc(sizeof(dog_t));
+	if (a == NULL)
+		return (NULL);
+
+	if (name == NULL)
+	{
+		free(a);
+		free(owner);
+		return (NULL);
+	}
+	if (owner == NULL)
+	{
+		free(a);
+		free(name);
+		return (NULL);
+	}
+	a->name = name;
+	a->age = age;
+	a->owner = owner;
+	return (a);
+}
