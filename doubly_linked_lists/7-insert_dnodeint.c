56789e01936e4a064870368d34282c71b5fea6b4
GinnaVionette <9011@holbertonstudents.com>
2024-03-26 11:25:40 -0400

    Exercise 7

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * insert_dnodeint_at_index - inserts new node at given position
+ * @h: pointer to direction
+ * @n: data integer
+ * @idx: position of new node
+ * Return: address to new node, or NULL if fail
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_mem, *res_mem = *h;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
+	{
+		if (res_mem == NULL)
+			return (NULL);
+		res_mem = res_mem->next;
+	}
+	if (res_mem->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new_mem = malloc(sizeof(dlistint_t));
+
+	if (new_mem == NULL)
+		return (NULL);
+
+	new_mem->n = n;
+	new_mem->next = res_mem->next;
+	new_mem->prev = res_mem;
+	res_mem->next->prev = new_mem;
+	res_mem->next = new_mem;
+
+	return (new_mem);
+}
